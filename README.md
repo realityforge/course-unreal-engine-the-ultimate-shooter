d9dd4da8bf1beeb8bae181a07c5526b7503aa663
Peter Donald <peter@realityforge.org>
2022-07-11 20:30:21 +1000

    Initial import of project
    Former-commit-id: f54b464235207385e19448a0153a3a4e2507e60c

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coursework for "Unreal Engine C++ The Ultimate Shooter Course"
+
+This contains the projects used for the course [Unreal Engine C++ The Ultimate Shooter Course](https://www.udemy.com/course/unreal-engine-the-ultimate-shooter-course/).
---
+++ Source/pelor/PelorGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PelorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PELOR_API APelorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
