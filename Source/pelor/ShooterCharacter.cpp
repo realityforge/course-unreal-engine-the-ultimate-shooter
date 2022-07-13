d69cf3d3b67cad4319bec33e8c31066a8a1b60ae
Peter Donald <peter@realityforge.org>
2022-07-13 16:55:29 +1000

    Whitespace
    Former-commit-id: bc8050ad76ae9d19775e91e108b53ca33d860eed

--- Source/pelor/ShooterCharacter.cpp
+++ Source/pelor/ShooterCharacter.cpp
@@ -6,9 +6,8 @@
 // Sets default values
 AShooterCharacter::AShooterCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
@@ -22,13 +21,10 @@ void AShooterCharacter::BeginPlay()
 void AShooterCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
-
