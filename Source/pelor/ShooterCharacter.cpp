b65135b7fdbff33098cebbe84475d7eebdb6d9fd
Peter Donald <peter@realityforge.org>
2022-07-14 10:38:27 +1000

    Lesson 12
    Former-commit-id: a15299d19800034dfd84484d2c524945ca9629d1

--- Source/pelor/ShooterCharacter.cpp
+++ Source/pelor/ShooterCharacter.cpp
@@ -2,12 +2,28 @@
 
 
 #include "ShooterCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 AShooterCharacter::AShooterCharacter()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// Create a Camera Boom (pulls in towards the character if there is a collision)
+	{
+		// Create new ActorSubobject named "CameraBoom" of type USpringArmComponent
+		CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+
+		// Attaches the CameraBoom to this character
+		CameraBoom->SetupAttachment(RootComponent);
+
+		// Attempt to keep 300 units behind the character (will shorten to avoid collisions)
+		CameraBoom->TargetArmLength = 300.0F;
+
+		// rotate the arm to match the controller
+		CameraBoom->bUsePawnControlRotation = true;
+	}
 }
 
 // Called when the game starts or when spawned
--- Source/pelor/ShooterCharacter.h
+++ Source/pelor/ShooterCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "ShooterCharacter.generated.h"
 
 UCLASS()
@@ -19,11 +20,23 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	// Using "class USpringArmComponent*" rather than
+	// "USpringArmComponent*" forward declares the type.
+	// which means we do not need to include header to declare
+	// this type
+	// UProperty puts it in "section" camera and allows access even though it is private etc
+	/** Camera Boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera, meta=(AllowPrivateAccess="true"))
+	class USpringArmComponent* CameraBoom;
+
+public:
+	FORCEINLINE USpringArmComponent* getCameraBoom() const { return CameraBoom; }
 };
