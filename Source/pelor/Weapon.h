2e3cfcef7bb4880045ecba34f38eadb9de802dce
Peter Donald <peter@realityforge.org>
2022-08-11 15:55:18 +1000

    Create Weapon actor
    Former-commit-id: 0f0ee941c2ce595f01e8fa2816c58d148c7215d7

---
+++ Source/pelor/Weapon.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Weapon.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class PELOR_API AWeapon : public AItem
+{
+	GENERATED_BODY()
+};
