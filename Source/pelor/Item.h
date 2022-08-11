8195d64f85efc3726e5f001d65ff75a7c15af9a0
Peter Donald <peter@realityforge.org>
2022-08-11 15:11:50 +1000

    Lesson 56: Start to define the Item actor
    Former-commit-id: e8fbada996c615d6bae79a1644fcca7896ae38e3

---
+++ Source/pelor/Item.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class PELOR_API AItem : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// private section for state for actor
+private:
+	/** Mesh representation for the item */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* ItemMesh;
+
+	/** Line trace collides with this box to show HUD widgets */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* CollisionBox;
+
+	// public section for accessors for state
+public:
+};
