15751edf736dd37afdf079391018a244fc2ba4b7
Peter Donald <peter@realityforge.org>
2022-08-13 00:12:47 +1000

    Enable UMG module
    Former-commit-id: 740bd4bc4f9fe8f2819f8585854ca910aed707bb

--- Source/pelor/pelor.Build.cs
+++ Source/pelor/pelor.Build.cs
@@ -7,8 +7,8 @@ public class pelor : ModuleRules
 	public pelor(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
+		PublicDependencyModuleNames.AddRange(new[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
