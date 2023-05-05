c3a0c266627f2457fa4128b08fcafb5745b76cac
Peter Donald <peter@realityforge.org>
2023-05-05 23:19:33 +1000

    Start driving Grux by animation blueprint

---
+++ Source/pelor/Private/GruxAnimInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Animation/AnimInstance.h"
+#include "CoreMinimal.h"
+#include "GruxAnimInstance.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class UGruxAnimInstance : public UAnimInstance
+{
+    GENERATED_BODY()
+};
