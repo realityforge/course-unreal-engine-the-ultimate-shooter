25588b213e0bc20321d35a044de1aee601cffe8d
Peter Donald <peter@realityforge.org>
2023-05-07 22:39:11 +1000

    Skip assets of an incorrect type

--- Content/Python/power_of_two_validator.py
+++ Content/Python/power_of_two_validator.py
@@ -4,6 +4,7 @@ import math
 def verify_power_of_two():
     # Get the libraries
     editor_util = unreal.EditorUtilityLibrary()
+    system_lib = unreal.SystemLibrary()
 
     selected_assets = editor_util.get_selected_assets()
     asset_count = len(selected_assets)
@@ -14,18 +15,17 @@ def verify_power_of_two():
     for asset in selected_assets:
         package_name = asset.get_package().get_name()
 
-        print(asset.get_class())
-        print(asset.get_class().get_name())
+        classname = system_lib.get_class_display_name(asset.get_class())
+        if 'Texture2D' == classname:
+            x_size = asset.blueprint_get_size_x()
+            y_size = asset.blueprint_get_size_y()
 
-        x_size = asset.blueprint_get_size_x()
-        y_size = asset.blueprint_get_size_y()
+            is_x_valid = math.log(x_size, 2).is_integer()
+            is_y_valid = math.log(y_size, 2).is_integer()
 
-        is_x_valid = math.log(x_size, 2).is_integer()
-        is_y_valid = math.log(y_size, 2).is_integer()
-
-        if not is_x_valid or not is_y_valid:
-            unreal.log("{} is not a power of two ({}, {})".format(package_name, x_size, y_size))
-            invalid_asset_count += 1
+            if not is_x_valid or not is_y_valid:
+                unreal.log("{} is not a power of two ({}, {})".format(package_name, x_size, y_size))
+                invalid_asset_count += 1
 
     if 1 == invalid_asset_count:
         unreal.log("1 texture identified that is not a power of two.")
