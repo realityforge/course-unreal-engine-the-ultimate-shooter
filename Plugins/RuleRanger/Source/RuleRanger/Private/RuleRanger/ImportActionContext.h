525135d4336fb04432e9eed7ae6675286e88b57d
Peter Donald <peter@realityforge.org>
2023-10-07 22:18:40 +1100

    Braid: Add mirror 'Plugins/RuleRanger' at 'dcca92a'

---
+++ Plugins/RuleRanger/Source/RuleRanger/Private/RuleRanger/ImportActionContext.h
@@ -0,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RuleRangerActionContext.h"
+#include "UObject/Object.h"
+#include "ImportActionContext.generated.h"
+
+/**
+ * Base class used to match against an object to determine whether a rule should be applied to the object.
+ */
+UCLASS(Blueprintable, BlueprintType)
+class RULERANGER_API UImportActionContext : public UObject, public IRuleRangerActionContext
+{
+    GENERATED_BODY()
+
+public:
+    virtual bool InErrorState() override;
+    virtual ERuleRangerActionTrigger GetActionTrigger() override;
+
+    void ResetContext(UObject* InObject, ERuleRangerActionTrigger InActionTrigger);
+
+private:
+    /** The object that the associated action is acting upon. */
+    UPROPERTY(VisibleAnywhere)
+    UObject* Object;
+
+    /** The reason that the associated action was triggered. */
+    UPROPERTY(VisibleAnywhere)
+    ERuleRangerActionTrigger ActionTrigger{ ERuleRangerActionTrigger::AT_Max };
+
+    /** Flag indicating the action triggered an error state. */
+    UPROPERTY(VisibleAnywhere)
+    bool bError;
+};
