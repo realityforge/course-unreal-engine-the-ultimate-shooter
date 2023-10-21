2d41f9ba749d0cafa656e06091dd8590b29475a5
Peter Donald <peter@realityforge.org>
2023-10-21 16:39:46 +1100

    Remove the historical validator infrastructure

--- Content/Python/init_unreal.py
+++
@@ -1,5 +0,0 @@
-import realityforge.assetlib.validator as validator
-import realityforge.scheme.asset_naming_registration as asset_naming_registration
-
-asset_naming_registration.load_rules()
-validator.setup()
